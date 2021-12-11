8e9f22cac51c5c47c1d1ceded300cf28676693da
Stormuke <fen1x@yandex.ru>
2021-12-11 18:05:51 +0300

    fix: alt

--- src/components/InfoTooltip.js
+++ src/components/InfoTooltip.js
@@ -3,7 +3,7 @@ function InfoTooltip(props) {
         <div className={`popup ${props.isOpen ? `popup_opened`: ""}`} onClick={props.onOverlayClick}>
             <div className="popup__container popup__container_size_m">
                 <button type="button" className="popup__button-close" onClick={props.onClose}/>
-                <img src={props.image} alt="" className="popup__picture"/>
+                <img src={props.image} alt="Картинка авторизации" className="popup__picture"/>
                 <h2 className="popup__title popup__title_theme_auth">{props.title}</h2>
             </div>
         </div>
